333dc067f0826be044ec92c4f05a98f2068bd7f8
Stephen <42769969+PBJF@users.noreply.github.com>
2019-05-07 20:51:12 +0300

    like emojis werent enough, we now have shibmotes
    :demonicpotat:

---
+++ Categories/shibmotes.js
@@ -0,0 +1,24 @@
+//this one was made by pbjf :P
+//might be broken
+//layout made by the meme factory tho
+//Copyright 2019
+
+//Variables
+var Command = require("../Util/command.js");
+var commands = [];
+
+//Commands
+commands.push(new Command(
+	"no",
+	"Shows the NO shibe!",
+	(client,msg) => {
+//how do i make the pic show up
+//we'll see in the next episode of GitHub
+	}
+));
+
+//Export
+module.exports = {
+	name: "Shibmotes (base)",
+	commands: commands
+};
